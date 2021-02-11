df9c179787bceff51b02c6a53b69a9094c603eb8
messin9 <mesisn9@gmail.com>
2021-02-12 00:10:45 +0300

    Initial Commit

---
+++ PhotoTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoTap
+//
+//  Created by Алексей on 12.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
